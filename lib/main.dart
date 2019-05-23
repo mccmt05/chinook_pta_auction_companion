e7e1cee9fc22e7add36c8a2c03afda59eea83cc3
mccmt05 <pshs14@yahoo.com>
2019-05-22 19:42:55 -0600

    Favorites list work

--- lib/main.dart
+++ lib/main.dart
@@ -60,50 +60,67 @@ class _MyAppState extends State<MyApp> {
       body: new ListView.builder(
         itemCount: groupings.length,
         itemBuilder: (context, i) {
-          return _buildIndivGroupingList(groupings[i]);
+          return _buildIndivGroupingList(groupings[i], false);
         },
       ),
     );
   }
 
-  Widget _buildIndivGroupingList(List<AuctionItem> itemList) {
+  Widget _buildIndivGroupingList(List<AuctionItem> itemList,  bool fromFavorites) {
     return ExpansionTile(
       title: new Text(itemList[0].itemGroup.groupName + "\nEnd Time: " + itemList[0].itemGroup.endTime,
         style: new TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.black) ,),
       backgroundColor: itemList[0].itemGroup.groupColor,
       children: <Widget>[
         new Column(
-          children: _buildExpandableContent(itemList),
+          children: _buildExpandableContent(itemList, fromFavorites),
         ),
       ],
       initiallyExpanded: true,
     );
   }
 
-  List<Widget> _buildExpandableContent(List<AuctionItem> itemList) {
+  List<Widget> _buildExpandableContent(List<AuctionItem> itemList, bool fromFavorites) {
     List<Widget> columnContent = [];
     for (AuctionItem item in itemList) {
       bool alreadySaved = _saved.contains(item);
-      columnContent.add(
-        new ListTile(
-          title: new Text(
-            "Item " + item.itemNum.toString() + ": " + item.itemTitle + "\nTeacher: " + item.itemTeacher + "\nValue: \$" + item.itemValue.toString() + "\nDescription: " + item.itemDescription,
-            style: new TextStyle(fontSize: 14.0, color: Colors.black),),
-          trailing: Icon(
-            alreadySaved ? Icons.favorite : Icons.favorite_border,
-            color: alreadySaved ? Colors.red : null,
+      if(fromFavorites) {
+        columnContent.add(
+          new ListTile(
+            title: new Text(
+              "Item " + item.itemNum.toString() + ": " + item.itemTitle +
+                  "\nTeacher: " + item.itemTeacher + "\nValue: \$" +
+                  item.itemValue.toString() + "\nDescription: " +
+                  item.itemDescription,
+              style: new TextStyle(fontSize: 14.0, color: Colors.black),),
           ),
-          onTap: () {
-            setState(() {
-              if (alreadySaved) {
-                _saved.remove(item);
-              } else {
-                _saved.add(item);
-              }
-            });
-          },
-        ),
-      );
+        );
+      }
+      else {
+        columnContent.add(
+          new ListTile(
+            title: new Text(
+              "Item " + item.itemNum.toString() + ": " + item.itemTitle +
+                  "\nTeacher: " + item.itemTeacher + "\nValue: \$" +
+                  item.itemValue.toString() + "\nDescription: " +
+                  item.itemDescription,
+              style: new TextStyle(fontSize: 14.0, color: Colors.black),),
+            trailing: Icon(
+              alreadySaved ? Icons.favorite : Icons.favorite_border,
+              color: alreadySaved ? Colors.red : null,
+            ),
+            onTap: () {
+              setState(() {
+                if (alreadySaved) {
+                  _saved.remove(item);
+                } else {
+                  _saved.add(item);
+                }
+              });
+            },
+          ),
+        );
+      }
     }
 
     return ListTile.divideTiles(context: context, tiles: columnContent).toList();
@@ -115,39 +132,42 @@ class _MyAppState extends State<MyApp> {
         builder: (BuildContext context) {
           List<AuctionItem> _savedSorted = _saved.toList();
           _savedSorted.sort((a, b) => a.itemNum.compareTo(b.itemNum));
-          /*
-          final List<Container> contentsOfFavorites = _savedSorted.map((AuctionItem item) {
-            return Container(color: item.itemGroup.groupColor, child: Text("Item " + item.itemNum.toString() + ": " + item.itemTitle + "\nTeacher: " + item.itemTeacher + "\nValue: \$" + item.itemValue.toString() + "\nDescription: " + item.itemDescription,),);
-          },);
-          */
 
-          final Iterable<ListTile> tiles = _savedSorted.map((AuctionItem item) {
-              return ListTile(
-                title: Text(
-                  "Item " + item.itemNum.toString() + ": " + item.itemTitle + "\nTeacher: " + item.itemTeacher + "\nValue: \$" + item.itemValue.toString() + "\nDescription: " + item.itemDescription,
-                ),
-
-              );
-            },
-          );
-
-          final List<Widget> divided = ListTile
-              .divideTiles(
-            context: context,
-            tiles: tiles,
-          )
-              .toList();
+          final List<List<AuctionItem>> favList = buildFavoritesList(_savedSorted);
 
           return Scaffold(
             appBar: AppBar(
               title: Text("Saved Auction Items", style: new TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.black),),
               backgroundColor: Colors.orange,
             ),
-            //body: ListView(children: contentsOfFavorites),
-            body: ListView(children: divided),
+            body: new ListView.builder(
+              itemCount: favList.length,
+              itemBuilder: (context, i) {
+                return _buildIndivGroupingList(favList[i], true);
+              },
+            ),
           );
         },
       ),
     );
+  }
+
+  List<List<AuctionItem>> buildFavoritesList(List<AuctionItem> inList) {
+    List<List<AuctionItem>> favoritesListSplit = [];
+    for(List<AuctionItem> indivList in groupings) {
+          favoritesListSplit.add(splitFavoriteList(inList, indivList[0].itemGroup));
+    }
+
+    return favoritesListSplit;
+  }
+
+  List<AuctionItem> splitFavoriteList(List<AuctionItem> inList, AuctionGrouping inGroup) {
+    List<AuctionItem> rtnList = [];
+    for(AuctionItem item in inList) {
+      if(item.itemGroup == inGroup) {
+        rtnList.add(item);
+      }
+    }
+    return rtnList;
   }
 }
